136f93548964df2f260bb1be7724dcfa75347569
Rambabu <rambabuakkapolu970@gmail.com>
2024-10-16 00:13:24 +0530

    Update and rename obstacle_avoidance_robot.ino to smart-rover.ino

--- smart-rover.ino
+++ smart-rover.ino
@@ -1,4 +1,4 @@
- /*obstacle avoidance, Bluetooth control robot */
+/* Smart Rover - AN obstacle avoidance& Bluetooth control robot */
 #include <Servo.h>
 #include <AFMotor.h>
 #define Echo A0
@@ -32,7 +32,6 @@ void setup() {
 void loop() {
   //Obstacle();
   Bluetoothcontrol();
-  //voicecontrol();
 }
 void Bluetoothcontrol() {
   if (Serial.available() > 0) {
@@ -76,39 +75,6 @@ void Obstacle() {
     }
   } else {
     forward();
-  }
-}
-void voicecontrol() {
-  if (Serial.available() > 0) {
-    value = Serial.read();
-    Serial.println(value);
-    if (value == '^') {
-      forward();
-    } else if (value == '-') {
-      backward();
-    } else if (value == '<') {
-      L = leftsee();
-      servo.write(spoint);
-      if (L >= 10 ) {
-        left();
-        delay(500);
-        Stop();
-      } else if (L < 10) {
-        Stop();
-      }
-    } else if (value == '>') {
-      R = rightsee();
-      servo.write(spoint);
-      if (R >= 10 ) {
-        right();
-        delay(500);
-        Stop();
-      } else if (R < 10) {
-        Stop();
-      }
-    } else if (value == '*') {
-      Stop();
-    }
   }
 }
 // Ultrasonic sensor distance reading function
